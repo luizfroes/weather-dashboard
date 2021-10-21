bfcff26595c2d209d068b2aec94cb182dc1c2723
Luiz Carlos Froes <luizfroes@gmail.com>
2021-10-21 15:47:57 +0100

    rendered current day weather card

--- assets/js/index.js
+++ assets/js/index.js
@@ -46,3 +46,36 @@ const mockData = {
     },
   ],
 };
+
+const dashboardContainer = $("#dashboard-container");
+
+renderCurrentWeatherCard = (currentData) => {
+  const currentWeatherCard = ` <div class="current-city-container">
+  <h2>
+    ${currentData.name} ${currentData.date}
+  </h2>
+  <div class="current-city-body">
+      <div class="current-city-content">
+        <p>Temp: ${currentData.temperature}&deg;F</p>
+        <p>Wind: ${currentData.wind} MPH</p>
+        <p>Humidity: ${currentData.humidity}%</p>
+        <p>
+          UV index: <span class="uv-btn">${currentData.uvi}</span>
+        </p>
+      </div>
+      <div class="current-city-img">
+        <img
+        src="https://openweathermap.org/img/w/${currentData.iconCode}.png"
+      />
+      </div>
+  </div>
+</div>`;
+
+  dashboardContainer.append(currentWeatherCard);
+};
+
+renderWeatherCards = (weatherData) => {
+  renderCurrentWeatherCard(weatherData.current);
+};
+
+renderWeatherCards(mockData);
